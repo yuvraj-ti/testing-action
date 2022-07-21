b29429f4081ac5ab47113d124fed566f2f6c0008
yuvraj-ti <108258091+yuvraj-ti@users.noreply.github.com>
2022-07-21 19:22:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.get('/healthcheck', (req, res) => {
+  res.send('healthcheck World!');
+});
+app.get('/.healthcheck', (req, res) => {
   res.send('healthcheck World!');
 });
 
