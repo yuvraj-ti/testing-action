110ae82656a78febfe3a533914862d75f005b7a4
yuvraj-ti <108258091+yuvraj-ti@users.noreply.github.com>
2022-07-21 18:20:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/healthcheck', (req, res) => {
+  res.send('healthcheck World!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
